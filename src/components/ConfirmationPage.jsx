0ad2c1b393f34334a91437a4d8e7702e001c5610
Clement Shum <clement.shum128@gmail.com>
2023-08-23 21:12:38 -0400

    updated confirmation pages

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,34 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import OrderConfirmation from "./OrderConfirmation"
+import OrderConfirmation from "./OrderConfirmation";
 
 function ConfirmationPage() {
   const { id } = useParams(); // Get the 'id' parameter from the URL
-  const [ order, setOrder ] = useState({})
+  const [order, setOrder] = useState({});
   // const getOrder = () => (
   //   fetch(`/api/orders/${id}`)
   //     .then((res) => res.json())
   //     .then((data) => setOrder(data))
   // )
 
-  const getOrder = async() => {
-    try{
+  const getOrder = async () => {
+    try {
       const response = await fetch(`/api/orders/${id}`);
       const data = await response.json();
       setOrder(data);
+    } catch (error) {
+      // console.log('Error: ', error)
     }
-    catch (error) {
-      console.log('Error: ', error)
-    }
-  }
+  };
 
   useEffect(() => {
-    getOrder()
-  }, [])
+    getOrder();
+  }, []);
 
-  return (
-    <OrderConfirmation order={order}/>
-  )
+  return <OrderConfirmation order={order} />;
 }
 
 export default ConfirmationPage;
